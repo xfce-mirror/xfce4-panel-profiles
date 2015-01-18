bdd09ac051507c4e28245ed8ec4a09cc623603af
Sean Davis <smd.seandavis@gmail.com>
2015-01-18 00:08:04 -0500

    Initial commit

---
+++ xfpanel-switch/panelconfig.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3
+# * Configuration switcher for xfce4-panel
+# *
+# * Copyright 2013 Alistair Buxton <a.j.buxton@gmail.com>
+# *
+# * License: This program is free software; you can redistribute it and/or
+# * modify it under the terms of the GNU General Public License as published
+# * by the Free Software Foundation; either version 3 of the License, or (at
+# * your option) any later version. This program is distributed in the hope
+# * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# * GNU General Public License for more details.
+
+from gi.repository import Gio, GLib
+import tarfile
+import io
+import time
+import os
+
+from os.path import expanduser
+
+# yes, python 3.2 has exist_ok, but it will still fail if the mode is different
+def mkdir_p(path):
+    try:
+        os.makedirs(path, exist_ok=True)
+    except FileExistsError:
+        pass
+
+def add_to_tar(t, bytes, arcname):
+    ti = tarfile.TarInfo(name=arcname)
+    ti.size = len(bytes)
+    ti.mtime = time.time()
+    f = io.BytesIO(bytes)
+    t.addfile(ti, fileobj=f)
+
+class PanelConfig(object):
+    def __init__(self):
+        self.desktops = []
+        self.properties = {}
+
+    def from_xfconf(xfconf):
+        pc = PanelConfig()
+
+        result = xfconf.call_sync('GetAllProperties', GLib.Variant('(ss)', ('xfce4-panel', '')), 0, -1, None)
+
+        props = result.get_child_value(0)
+
+        for n in range(props.n_children()):
+            p = props.get_child_value(n)
+            pp = p.get_child_value(0).get_string()
+            pv = p.get_child_value(1).get_variant()
+
+            pn = GLib.Variant.parse(None, str(pv), None, None)
+            assert(pv == pn)
+
+            pc.properties[pp] = pv
+
+        pc.find_desktops()
+
+        pc.source = None
+
+        return pc
+
+    def from_file(filename):
+        pc = PanelConfig()
+
+        pc.source = tarfile.open(filename, mode='r')
+        config = pc.source.extractfile('config.txt')
+
+        for line in config:
+            x = line.decode('utf-8').strip().split(' ', 1)
+            pc.properties[x[0]] = GLib.Variant.parse(None, x[1], None, None)
+
+        pc.find_desktops()
+
+        return pc
+
+    def find_desktops(self):
+        for pp,pv in self.properties.items():
+            path = pp.split('/')
+            if len(path) == 3 and path[0] == '' and path[1] == 'plugins' and path[2].startswith('plugin-'):
+                number = path[2].split('-')[1]
+                if pv.get_type_string() == 's' and pv.get_string() == 'launcher':
+                   for d in self.properties['/plugins/plugin-'+number+'/items'].unpack():
+                       self.desktops.append('launcher-'+number+'/'+d)
+
+    def get_desktop_source_file(self, desktop):
+        if self.source == None:
+            return open(expanduser("~")+'/.config/xfce4/panel/'+desktop, 'rb')
+        else:
+            return self.source.extractfile(desktop)
+
+    def to_file(self, filename):
+        if filename.endswith('.gz'):
+            mode = 'w:gz'
+        elif filename.endswith('.bz2'):
+            mode = 'w:bz2'
+        else:
+            mode = 'w'
+        t = tarfile.open(name=filename, mode=mode)
+        props_tmp = []
+        for (pp,pv) in sorted(self.properties.items()):
+            props_tmp.append(str(pp)+' '+str(pv))
+        add_to_tar(t, '\n'.join(props_tmp).encode('utf8'), 'config.txt')
+
+        for d in self.desktops:
+            bytes = self.get_desktop_source_file(d).read()
+            add_to_tar(t, bytes, d)
+
+        t.close()
+
+    def to_xfconf(self, xfconf):
+        os.system('killall xfce4-panel')
+
+        for (pp,pv) in sorted(self.properties.items()):
+            result = xfconf.call_sync('SetProperty', GLib.Variant('(ssv)', ('xfce4-panel', pp, pv)), 0, -1, None)
+
+        panel_path = expanduser("~")+'/.config/xfce4/panel/'
+        for d in self.desktops:
+            bytes = self.get_desktop_source_file(d).read()
+            mkdir_p(panel_path+os.path.dirname(d))
+            f = open(panel_path+d, 'wb')
+            f.write(bytes)
+            f.close()
+
+        os.system('cd ~ && xfce4-panel &')
+
+
+
+if __name__=='__main__':
+
+    import sys
+
+    session_bus = Gio.BusType.SESSION
+    cancellable = None
+    connection = Gio.bus_get_sync(session_bus, cancellable)
+
+    proxy_property = 0
+    interface_properties_array = None
+    destination = 'org.xfce.Xfconf'
+    path = '/org/xfce/Xfconf'
+    interface = destination
+
+    xfconf = Gio.DBusProxy.new_sync(
+         connection,
+         proxy_property,
+         interface_properties_array,
+         destination,
+         path,
+         interface,
+         cancellable)
+
+    if len(sys.argv) != 3 or sys.argv[1] not in ['load', 'save']:
+        print('Panel Switch v0.1 - Usage:')
+        print(sys.argv[0]+' save <filename> : save current configuration.')
+        print(sys.argv[0]+' load <filename> : load configuration from file.')
+        print('')
+        exit(-1)
+
+    if sys.argv[1] == 'save':
+        PanelConfig.from_xfconf(xfconf).to_file(sys.argv[2])
+    elif sys.argv[1] == 'load':
+        PanelConfig.from_file(sys.argv[2]).to_xfconf(xfconf)
+
---
+++ xfpanel-switch/xfpanel-switch.py
@@ -0,0 +1,179 @@
+#!/usr/bin/python
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+#
+#   This program is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU General Public License version 3, as published
+#   by the Free Software Foundation.
+#
+#   This program is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranties of
+#   MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+#   PURPOSE.  See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along
+#   with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import gettext
+gettext.textdomain('xfpanel-switch')
+
+from gettext import gettext as _
+from gettext import ngettext
+
+import shlex
+import os
+import datetime
+from gi.repository import Gtk, GLib, Gio
+
+from panelconfig import PanelConfig
+
+
+class XfpanelSwitch:
+    '''XfpanelSwitch application class.'''
+
+    data_dir = "xfpanel-switch"
+    save_location = os.path.join(GLib.get_user_data_dir(), data_dir)
+
+    def __init__(self):
+        '''Initialize the Xfce Panel Switch application.'''
+        self.builder = Gtk.Builder()
+        self.builder.set_translation_domain ('xfpanel-switch')
+
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        glade_file = os.path.join(script_dir, "xfpanel-switch.glade")
+        self.builder.add_from_file(glade_file)
+        self.builder.connect_signals(self)
+
+        self.window = self.builder.get_object("xfpanel_switch_window")
+        self.window.set_title(_("Xfce Panel Switch"))
+        self.fix_xfce_header()
+
+        self.load_xfconf()
+
+        self.treeview = self.builder.get_object('saved_configurations')
+        self.tree_model = self.treeview.get_model()
+        for config in self.get_saved_configurations():
+            self.tree_model.append(config)
+
+        self.window.show()
+
+    def load_xfconf(self):
+        session_bus = Gio.BusType.SESSION
+        cancellable = None
+        connection = Gio.bus_get_sync(session_bus, cancellable)
+
+        proxy_property = 0
+        interface_properties_array = None
+        destination = 'org.xfce.Xfconf'
+        path = '/org/xfce/Xfconf'
+        interface = destination
+
+        self.xfconf = Gio.DBusProxy.new_sync(
+             connection,
+             proxy_property,
+             interface_properties_array,
+             destination,
+             path,
+             interface,
+             cancellable)
+
+    def fix_xfce_header(self):
+        ''' Set background-color of frame to base-color to make it resemble the
+        XfceHeading widget '''
+        self.xfce_header = self.builder.get_object("xfce_header")
+        entry = Gtk.Entry.new()
+        style = entry.get_style_context()
+        base_color = style.lookup_color("theme_base_color")
+        self.xfce_header.override_background_color(0, base_color[1])
+        fg_color = style.lookup_color("theme_fg_color")
+        self.xfce_header.override_color(0, fg_color[1])
+
+    def get_data_dirs(self):
+        dirs = []
+        for directory in GLib.get_system_data_dirs():
+            path = os.path.join(directory, self.data_dir)
+            if os.path.isdir(path):
+                dirs.append(path)
+        path = os.path.join(GLib.get_user_data_dir(), self.data_dir)
+        if os.path.isdir(path):
+            dirs.append(path)
+        return dirs
+
+    def get_saved_configurations(self):
+        results = []
+        results.append(("", "Current Configuration", ""))
+
+        for directory in self.get_data_dirs():
+            for filename in os.listdir(directory):
+                name, ext = os.path.splitext(filename)
+                name, tar = os.path.splitext(name)
+                if ext in [".gz", ".bz2"]:
+                    path = os.path.join(directory, filename)
+                    t = os.path.getmtime(path)
+                    datetime_o = datetime.datetime.fromtimestamp(t)
+                    modified = datetime_o.strftime("%x")
+                    results.append((path, name, modified))
+
+        return results
+
+    def get_selected(self):
+        model, treeiter = self.treeview.get_selection().get_selected()
+        values = model[treeiter][:]
+        return (model, treeiter, values)
+
+    def get_save_dialog(self):
+        dialog = Gtk.Dialog.new()
+        dialog.set_title("Save configuration name as...")
+        dialog.set_size_request(300,100)
+        dialog.add_buttons("Cancel", Gtk.ResponseType.CANCEL,
+                           "Save", Gtk.ResponseType.OK)
+        content_area = dialog.get_content_area()
+        self.name_entry = Gtk.Entry.new()
+        content_area.add(self.name_entry)
+        content_area.show_all()
+
+        return dialog
+
+    def save_configuration(self, name):
+        filename = name + ".tar.bz2"
+        filename = os.path.join(self.save_location, filename)
+        PanelConfig.from_xfconf(self.xfconf).to_file(filename)
+        created = datetime.datetime.now().strftime("%X")
+        self.tree_model.append([filename, name, created])
+
+    def on_save_clicked(self, widget):
+        dialog = self.get_save_dialog()
+        if dialog.run() == Gtk.ResponseType.OK:
+            name = self.name_entry.get_text().strip()
+            if len(name) > 0:
+                self.save_configuration(name)
+        dialog.destroy()
+
+    def load_configuration(self, filename):
+        if os.path.isfile(filename):
+            PanelConfig.from_file(filename).to_xfconf(self.xfconf)
+
+    def on_apply_clicked(self, widget):
+        model, treeiter, values = self.get_selected()
+        filename = values[0]
+        self.load_configuration(filename)
+
+    def delete_configuration(self, filename):
+        os.remove(filename)
+
+    def on_delete_clicked(self, widget):
+        model, treeiter, values = self.get_selected()
+        filename = values[0]
+        self.delete_configuration(filename)
+        model.remove(treeiter)
+
+    def on_window_destroy(self, *args):
+        '''Exit the application when the window is closed.'''
+        Gtk.main_quit()
+
+    def on_close_clicked(self, *args):
+        '''Exit the application when the window is closed.'''
+        Gtk.main_quit()
+
+if __name__ == "__main__":
+    main = XfpanelSwitch()
+    Gtk.main()
